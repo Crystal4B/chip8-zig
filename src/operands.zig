c4a797d992576a021fc160f1802fc20fc75bc2b6
Marcin Sęk <marcin.j.sek@gmail.com>
2025-01-28 00:44:44 +0100

    test: added utils & operand tests

--- src/operands.zig
+++ src/operands.zig
@@ -22,20 +22,75 @@ pub const Operand = struct {
 pub const OperandDecoder = fn (u16) void;
 
 // Chip8 Operand Decoding
+test "REGXY layout has register x at 2nd nibble and y on the 3rd" {
+    const opcode: u16 = 0xA2F3;
+    const operand = REGXY(opcode);
+
+    try std.testing.expect(operand.Layout == OperandLayout.REGXY);
+    try std.testing.expect(operand.XReg == 0x2);
+    try std.testing.expect(operand.YReg == 0xF);
+
+    try std.testing.expect(operand.Abs == null);
+}
+
 pub fn REGXY(opcode: u16) Operand {
     return Operand{ .XReg = utils.readNibble(opcode, 1, 1), .YReg = utils.readNibble(opcode, 2, 1), .Layout = OperandLayout.REGXY };
+}
+
+test "REGV layout has 1 register at 2nd nibble and an absolute value at the lower byte" {
+    const opcode: u16 = 0xBF32;
+    const operand = REGV(opcode);
+
+    try std.testing.expect(operand.Layout == OperandLayout.REGV);
+    try std.testing.expect(operand.XReg == 0xF);
+    try std.testing.expect(operand.Abs == 0x32);
+
+    try std.testing.expect(operand.YReg == null);
 }
 
 pub fn REGV(opcode: u16) Operand {
     return Operand{ .XReg = utils.readNibble(opcode, 1, 1), .Abs = utils.readNibble(opcode, 2, 2), .Layout = OperandLayout.REGV };
 }
 
+test "REG layout has 1 register at the 2nd nibble" {
+    const opcode: u16 = 0x2FD5;
+    const operand = REG(opcode);
+
+    try std.testing.expect(operand.Layout == OperandLayout.REG);
+    try std.testing.expect(operand.XReg == 0xF);
+
+    try std.testing.expect(operand.YReg == null);
+    try std.testing.expect(operand.Abs == null);
+}
+
 pub fn REG(opcode: u16) Operand {
     return Operand{ .XReg = utils.readNibble(opcode, 1, 1), .Layout = OperandLayout.REG };
 }
 
+test "ABS layout only contains an absolute value" {
+    const opcode: u16 = 0x7ABC;
+    const operand = ABS(opcode);
+
+    try std.testing.expect(operand.Layout == OperandLayout.ABS);
+    try std.testing.expect(operand.Abs == 0xABC);
+
+    try std.testing.expect(operand.XReg == null);
+    try std.testing.expect(operand.YReg == null);
+}
+
 pub fn ABS(opcode: u16) Operand {
     return Operand{ .Abs = utils.readNibble(opcode, 1, 3), .Layout = OperandLayout.ABS };
+}
+
+test "NONE layout has no operand at all" {
+    const opcode: u16 = 0xABCD;
+    const operand = NONE(opcode);
+
+    try std.testing.expect(operand.Layout == OperandLayout.NONE);
+
+    try std.testing.expect(operand.XReg == null);
+    try std.testing.expect(operand.YReg == null);
+    try std.testing.expect(operand.Abs == null);
 }
 
 pub fn NONE(_: u16) Operand {
--- src/utils.zig
+++ src/utils.zig
@@ -1,7 +1,25 @@
 const std = @import("std");
 
+test "mergeBytes combines two u8s into a u16" {
+    const expected: u16 = 0xA1B2;
+    const result = mergeBytes(0xA1, 0xB2);
+
+    try std.testing.expect(result == expected);
+}
+
 pub fn mergeBytes(high: u8, low: u8) u16 {
     return @as(u16, high) << 8 | @as(u16, low);
+}
+
+test "readNibble extracts specified nibble from u16" {
+    const base: u16 = 0xA1B2;
+
+    try std.testing.expectEqual(0xA1B2, readNibble(base, 0, 4));
+    try std.testing.expectEqual(0xA1B, readNibble(base, 0, 3));
+    try std.testing.expectEqual(0x1B, readNibble(base, 1, 2));
+    try std.testing.expectEqual(0xB2, readNibble(base, 2, 2));
+    try std.testing.expectEqual(0xA, readNibble(base, 0, 1));
+    try std.testing.expectEqual(0xB, readNibble(base, 2, 1));
 }
 
 pub fn readNibble(value: u16, start: u4, len: u4) u16 {
